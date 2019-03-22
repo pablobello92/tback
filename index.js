75727d716a7c3013fef42b5aa5d26139f9d9fc7f
Pablo Bello <77soft67@gmail.com>
2019-03-22 14:46:09 -0300

    Middleware Morgan: handler de requests HTTP

--- index.js
+++ index.js
@@ -3,8 +3,21 @@
 const express = require('express');
 const app = express();
 
+const morgan = require('morgan');
+app.use(morgan('dev'));
+
+// Routes
+
 app.get('/', (req, res) => {
     res.end('Hello World!');
+});
+
+app.get('/login', (req, res) => {
+    res.end('Login!');
+});
+
+app.get('*', (req, res) => {
+    res.end('Route not found!');
 });
 
 app.listen(3000, () => {
