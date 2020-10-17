944f02d90d760d989bc32dc8a933fb21b6639d2b
pablo-bello <77soft67@gmail.com>
2020-10-16 23:26:14 -0300

    User update endpoint

--- controllers/users.js
+++ controllers/users.js
@@ -3,13 +3,24 @@ const Users = require('./../models/user');
 
 const getUserCallback = (req, res) => {
     Users.findOne({ username: req.query.username})
-    .then( user => {
+    .then(user => {
         res.send(user);
     })
-    .catch( err => {
+    .catch(err => {
         console.error(err);
     });
 }
 
-module.exports = getUserCallback;
+const updateUserCallback = (req, res) => {
+    const user = req.body;
+    Users.update(user)
+    .then(user => {
+        res.send(["model updated!"]);
+    })
+    .catch(err => {
+        res.send(["Error!"]);
+    });
+}
+
+module.exports = [ getUserCallback, updateUserCallback ]
 
--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-
-const predictRoads = require('../controllers/predictions');
-
 const [ indexCallback, notFoundCallback ] = require ('./../controllers/index');
 const loginCallBack = require('./../controllers/login');
-const getUserCallback = require('../controllers/users');
+const [ getUserCallback, updateUserCallback ] = require('../controllers/users');
 const getCitiesCallback = require('../controllers/cities');
 const [ getTracksCallback, sumarizeTracksCallback] = require('./../controllers/tracks');
 const [ getReparationsCallback, putReparationCallback ] = require('../controllers/reparations');
@@ -14,12 +11,13 @@ const [ predictRoadsCallback, predictAnomaliesCallback ] = require('../controlle
 
 router.get('/', indexCallback);
 router.get('/api/login', loginCallBack);
-router.get('/api/users/', getUserCallback );
+router.get('/api/users', getUserCallback );
+router.put('/api/users/update', updateUserCallback );
 router.get('/api/cities/', getCitiesCallback);
 router.get('/api/tracks', getTracksCallback);
 router.get('/api/tracks/sumarize', sumarizeTracksCallback);
 router.get('/api/reparations', getReparationsCallback);
-router.put('/api/reparations', putReparationCallback);
+router.put('/api/reparations/insert', putReparationCallback);
 
 router.get('/api/predictions/anomalies', predictAnomaliesCallback);
 router.get('/api/predictions/roadTypes', predictRoadsCallback);
