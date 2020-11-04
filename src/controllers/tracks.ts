4f454a974b4c44ce59f4ca0dd1bc7696204c3071
pablo-bello <77soft67@gmail.com>
2020-11-04 14:23:57 -0300

    WIP: mappings for sumarizations and predictions

--- src/controllers/tracks.ts
+++ src/controllers/tracks.ts
@@ -1,38 +1,57 @@
 export {};
 
 import Track from './../models/track';
-import { Observable, from } from 'rxjs';
+import { Observable, from, forkJoin } from 'rxjs';
 import { Document } from 'mongoose';
 import { fetchCities } from './cities';
-import { map, switchMap, tap } from 'rxjs/operators';
+import { map, mergeAll, mergeMap, switchMap, tap } from 'rxjs/operators';
 
 
 export const fetchTracks = (filter: {} = {}, fields: string, skip: number, limit: number): Observable<Document[]> =>
 	from(Track.find(filter).select(fields).skip(skip).limit(limit).exec());
 
-export const getTracksMapByCity = (): void => {
-	fetchCities()
+const getTracksMappingByCity = (city: string, fields: string, skip: number, limit: number): Observable<any> => {
+	return fetchTracks({city: city}, fields, skip, limit)
+	.pipe(
+		map(tracks => {
+			return {
+				city: city,
+				startTime: Date.parse(new Date().toDateString()),
+				tracks: tracks
+			};
+		})
+	);
+}
+
+//TODO: Remove LIMIT, add accelerometers!
+//TODO: fix deprecation warning on forkJoin
+export const getTracksMapByCity = (): Observable<any> => {
+	return fetchCities()
 	.pipe(
 		map((cities: Document[]) => cities.map((city: any) => city.name)),
-		map((cityNames: string[]) => {
-			return cityNames.map((item: string) => {
-				return {
-					city: item,
-					startTime: Date.parse(new Date().toDateString()),
-					tracks: []
-				};
-			});
+		mergeMap((cities: string[]) => {
+			const observables = cities.map((city: any) =>  getTracksMappingByCity(city, 'id city startTime ranges', 0, 10));
+			return forkJoin(...observables);
 		})
-		/*switchMap((cities: string[]) => {
-			const observables = cities.map((city: string) => );
-		})*/
-	)
-	.subscribe((objects: any[]) => {
-		console.log(objects);
-	});
+	);
 }
 
 export const getTracksCallback = (req: any, res: any): void => {
+	getTracksMapByCity()
+	.subscribe({
+        next(result: any[]) {
+			console.clear();			
+			result.forEach((item: any) => {
+				console.log(item.city);
+				console.log(item.startTime);
+				console.log(item.tracks);
+			});
+        },
+        error(err: any) { 
+            console.error(err);
+        }
+    });
+
 	const filter = {
 		username: req.query.username,
 		city: req.query.city,
