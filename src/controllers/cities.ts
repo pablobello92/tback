060546673ff5f1557e8619a4e7067dbd4c1b78a5
pablobello92 <77soft67@gmail.com>
2021-08-16 21:33:06 -0300

    Refactor: only one City retriever left

--- src/controllers/cities.ts
+++ src/controllers/cities.ts
@@ -1,13 +1,10 @@
 export {};
 
 import City from './../models/city';
-import {
-    Document
-} from 'mongoose';
 
 export const getCitiesCallback = (req: any, res: any): void => {
-    fetchCities()
-    .then((result: Document[]) => {
+    fetchCityFields()
+    .then((result: any[]) => {
         res.send(result);
     })
     .catch((error: any) => {
@@ -15,6 +12,4 @@ export const getCitiesCallback = (req: any, res: any): void => {
     });
 };
 
-export const fetchCities = (): Promise<Document[]> => City.find({}).exec();
-
-export const fetchCityFields = (fields: string): Promise<any[] > => City.find().lean().select(fields).exec();
+export const fetchCityFields = (fields?: string): Promise<any[] > => City.find().lean().select(fields).exec();
